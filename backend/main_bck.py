46cb9879058b4373d5713eb3a44337fccb3fb0fd
SirEagIe <egor2002-1002@mail.ru>
2024-09-18 13:23:18 +0000

    added detection mode

--- backend/main_bck.py
+++ backend/main_bck.py
@@ -15,7 +15,7 @@ redis = Redis(host='redis', port=6379)
 
 
 @celery_client.task
-def sniff_flows():
+def sniff_flows(mode):
     os.remove('flows.csv')
     connections = {}
     def packet_process(pkt):
@@ -23,7 +23,7 @@ def sniff_flows():
             with open('flows.csv', 'a') as file:
                 for conn in connections.values():
                     file.write(conn.get_row() + '\n')
-            train()
+            if mode == 'train': train()
             exit(0)
         if pkt.haslayer(IP) and (pkt.haslayer(TCP) or pkt.haslayer(UDP)):
             is_forward = int(pkt[IP].src.replace('.', '')) < int(pkt[IP].dst.replace('.', ''))
@@ -37,6 +37,7 @@ def sniff_flows():
                     conn.close()
                     with open('flows.csv', 'a') as file:
                         file.write(conn.get_row() + '\n')
+                        if mode == 'detect': detect(conn.get_row())
                     connections.pop(f)
                     conn = Connection(pkt)
                     connections[f] = conn
@@ -46,6 +47,7 @@ def sniff_flows():
                         conn.close()
                         with open('flows.csv', 'a') as file:
                             file.write(conn.get_row() + '\n')
+                            if mode == 'detect': detect(conn.get_row())
                         connections.pop(f)
             else:
                 conn = Connection(pkt)
@@ -53,47 +55,6 @@ def sniff_flows():
     sniffer = sniff(prn=packet_process)
 
 import random
-
-@celery_client.task
-def sniff_flows_detect():
-    os.remove('flows.csv')
-    connections = {}
-    def packet_process(pkt):
-        if redis.get('started') == b'false':
-            with open('flows.csv', 'a') as file:
-                for conn in connections.values():
-                    file.write(conn.get_row() + '\n')
-            train()
-            exit(0)
-        if pkt.haslayer(IP) and (pkt.haslayer(TCP) or pkt.haslayer(UDP)):
-            is_forward = int(pkt[IP].src.replace('.', '')) < int(pkt[IP].dst.replace('.', ''))
-            if is_forward:
-                f = f'{pkt[IP].src}-{pkt[IP].dst}-{pkt[IP].sport}-{pkt[IP].dport}-{6 if pkt.haslayer(TCP) else 17}'
-            else:
-                f = f'{pkt[IP].dst}-{pkt[IP].src}-{pkt[IP].dport}-{pkt[IP].sport}-{6 if pkt.haslayer(TCP) else 17}'
-            conn = connections.get(f)
-            if conn:
-                if pkt.time - conn.start_time > 120:
-                    conn.close()
-                    with open('flows.csv', 'a') as file:
-                        file.write(conn.get_row() + '\n')
-                        detect(conn.get_row())
-                    connections.pop(f)
-                    conn = Connection(pkt)
-                    connections[f] = conn
-                else:
-                    conn.add_packet(pkt)
-                    if pkt.haslayer(TCP) and ('F' in pkt[TCP].flags and conn.get_total_packets() > 1):
-                        conn.close()
-                        with open('flows.csv', 'a') as file:
-                            file.write(conn.get_row() + '\n')
-                            detect(conn.get_row())
-                        connections.pop(f)
-            else:
-                conn = Connection(pkt)
-                connections[f] = conn
-    sniffer = sniff(prn=packet_process)
-
 
 def train():
     clf = None
