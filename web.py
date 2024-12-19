23f52641abaed27550897a5ce51e40dbbc0b417c
sireagie <sireagie>
2024-12-19 11:08:33 +0000

    web

--- web.py
+++ web.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect
 from sklearn.neural_network import MLPClassifier
 import sqlite3
+from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 db = sqlite3.connect('ids.db')
@@ -9,7 +10,6 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS incidents (
     id INTEGER PRIMARY KEY,
     src TEXT,
     dst TEXT,
-    sessions_num INTEGER,
     start_time INTEGER,
     label TEXT
 )''')
@@ -32,14 +32,14 @@ def incidents():
 def incidents_data():
     db = sqlite3.connect('ids.db')
     cursor = db.cursor()
-    cursor.execute("""SELECT src, dst, count(*), min(start_time), label as start_time FROM sessions GROUP BY src, dst, label HAVING count(*) > 10 AND label NOT LIKE 'BENIGNa'""")
+    cursor.execute("""SELECT src, dst, min(start_time), label as start_time FROM sessions GROUP BY src, dst, label HAVING count(*) > 10 AND label NOT LIKE 'BENIGN'""")
     data = cursor.fetchall()
     for d in data:
-        cursor.execute(f"""SELECT * FROM incidents WHERE src='{d[0]}' AND dst='{d[1]}' AND sessions_num={d[2]} AND start_time={d[3]} AND label='{d[4]}'""")
+        cursor.execute(f"""SELECT * FROM incidents WHERE src='{d[0]}' AND dst='{d[1]}' AND start_time={d[2]} AND label='{d[3]}'""")
         if not cursor.fetchall():
-            cursor.execute(f"""INSERT INTO incidents (src, dst, sessions_num, start_time, label) VALUES ('{d[0]}', '{d[1]}', {d[2]}, {d[3]}, '{d[4]}');""")
+            cursor.execute(f"""INSERT INTO incidents (src, dst, start_time, label) VALUES ('{d[0]}', '{d[1]}', {d[2]}, '{d[3]}');""")
         db.commit()
-    cursor.execute("""SELECT id, src, dst, sessions_num, datetime(start_time, 'unixepoch') as start_time, label FROM incidents ORDER BY id DESC""")
+    cursor.execute("""SELECT id, src, dst, datetime(start_time, 'unixepoch') as start_time, label FROM incidents ORDER BY id DESC""")
     data = cursor.fetchall()
     db.close()
     return {'data': data}
@@ -69,5 +69,17 @@ def data_malicious():
         'data': [dict(zip(header, d)) for d in data]
     }
 
+def rotate_db():
+    incidents_data()
+    db = sqlite3.connect('ids.db')
+    cursor = db.cursor()
+    cursor.execute("""DELETE FROM sessions WHERE start_time < (julianday('now') - 2440587.5) * 86400.0 - 3600""")
+    db.commit()
+    cursor.execute("""SELECT start_time,(julianday('now') - 2440587.5) * 86400.0 FROM sessions""")
+    db.close()
+
 if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(rotate_db, trigger='interval', id='rotate_db', seconds=3600)
+    scheduler.start()
     app.run(debug=True, host='0.0.0.0', port='8888')
